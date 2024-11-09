67f196a102b3155790b14aab4f20d5c8abe86a62
laithkh12 <168769648+laithkh12@users.noreply.github.com>
2024-11-09 15:28:21 +0200

    Update main.py

--- Open Weather Map/main.py
+++ Open Weather Map/main.py
@@ -9,6 +9,8 @@ OWM_Endpoint = os.getenv("OWM_ENDPOINT")
 API_KEY = os.getenv("API_KEY")
 ACCOUNT_SID = os.getenv("ACCOUNT_SID")
 AUTH_TOKEN = os.getenv("AUTH_TOKEN")
+FROM_PHONE = os.getenv("FROM_PHONE")
+TO_PHONE = os.getenv("TO_PHONE")
 
 weather_params = {
     "lat":31.046051,
@@ -29,6 +31,7 @@ for hour_data in weather_data["list"]:
         will_rain = True
 if will_rain:
     client = Client(ACCOUNT_SID, AUTH_TOKEN)
-    message = client.messages.create(body="It's going to rain today. Remember to bring an umbrella.", from_="+13135280356", to="+972542392090")
+    message = client.messages.create(body="It's going to rain today. Remember to bring an umbrella.", from_=FROM_PHONE,
+        to=TO_PHONE)
 
     print(message.status)
